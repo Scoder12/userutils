5bb6444acae573ba88606cc53084c91a076796ed
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2018-04-27 12:45:02 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mathextras',
+      version='0.1',
+      description='Helps work with human users. ',
+      url='https://github.com/Scoder12/userutils',
+      author='Scoder12 and Poipt',
+      author_email='pymathextras@gmail.com',
+      license='MIT',
+      packages=['userutils'],
+zip_safe=False)
