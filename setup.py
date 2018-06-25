7d565c314de9d52bab2a0c9059302f7cd497c6b2
Scoder12 <34356756+Scoder12@users.noreply.github.com>
2018-06-25 15:17:48 -0700

    Fix name mathextras to userutils

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
-setup(name='mathextras',
-      version='0.1',
+setup(name='userutils',
+      version='0.1.0',
       description='Helps work with human users. ',
       url='https://github.com/Scoder12/userutils',
       author='Scoder12 and Poipt',
